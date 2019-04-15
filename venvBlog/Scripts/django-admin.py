b4d9f9704802ba628af92a42986e964ff31f09cb
Dayud Mohamed Farah <voiphandy@gmail.com>
2019-04-15 19:28:18 +0300

    Deploying to pythonanywhere

---
+++ venvBlog/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jinka\documents\projects\coreyschafer\venvblog\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
